9f0d21a40392f1b71f613d045f3ac684324f90f0
unknown <sandinista89@yandex.ru>
2022-06-27 11:39:40 +0300

    2 task was added

--- HW_2_JS.js
+++ HW_2_JS.js
@@ -18,3 +18,27 @@ for(count = 1; count<=grade; count++ ){
 };
 }
 taskTwo(4)
+
+//2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
+//Пример в консоли:
+//:)
+//:):)
+//:):):)
+//:):):):)
+//:):):):):)
+
+let smile = ':)';
+for(count = 1; count <=5 ; count++){
+    b = smile.repeat(count)
+    console.log(b)
+}
+
+//2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода
+//e.g. function printSmile(stroka, numberOfRows)
+
+function printSmile(stroka, numberOfRows){
+    for(count = 1 ; count <=numberOfRows ; count++){
+        d = String(stroka).repeat(count)
+        console.log(d, count)
+    }};
+printSmile('name', 6)
