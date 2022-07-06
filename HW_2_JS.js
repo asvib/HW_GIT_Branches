c38b2e63962c2b5af27e371af4e0e67435e3ed79
unknown <sandinista89@yandex.ru>
2022-07-06 15:42:04 +0300

    task 4 was added

--- HW_2_JS.js
+++ HW_2_JS.js
@@ -68,3 +68,17 @@ getWordStructure('Check-list');
 getWordStructure('case');
 getWordStructure('Case');
 
+//4**. Написать функцию, которая проверяет, является ли слово палиндромом
+//e.g. function isPalindrom(word)
+//Проверки: 'abba', 'Abba'
+
+function isPalindrom(word){
+    word = word.toLowerCase()
+    for(let i=0, j=word.length-1; i<word.length, j>=0; i++, j--){
+        if(word[i] != word[j]){
+            return false
+        }
+    }
+    return true
+};
+console.log(isPalindrom('Abba'));
